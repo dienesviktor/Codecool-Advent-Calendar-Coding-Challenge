995fae16f7e187fa9c2976d34d2546da96f322ac
dienesviktor <dienesviktor@gmail.com>
2021-12-05 13:36:44 +0100

    add evaluate actions function

--- santabot.py
+++ santabot.py
@@ -1,13 +1,15 @@
-karma = {}
+def evaluate_actions(file):
+    rated_actions = {}
+    with open(file, "r") as file:
+        lines = file.read().splitlines()
+        splitted = [lines[i].split(";") for i in range(1, len(lines))]
+        for key in splitted:
+            type = key[0]
+            action = key[1]
+            if type not in rated_actions:
+                rated_actions[type] = [action]
+            else:
+                rated_actions[type].append(action)
+    return rated_actions
 
-with open("karma.csv", "r") as file:
-    lines = file.read().splitlines()
-    splitted = [lines[i].split(";") for i in range(1, len(lines))]
-    
-    for key in splitted:
-        type = key[0]
-        action = key[1]
-        if type not in karma:
-            karma[type] = [action]
-        else:
-            karma[type].append(action)
+print(evaluate_actions("karma.csv"))
