1b64d12095be7799a0f20fd36d7bcf4916002a7c
dievik <dienesviktor@gmail.com>
2021-12-08 22:06:01 +0100

    update santabot with new functions

--- santabot.py
+++ santabot.py
@@ -1,7 +1,14 @@
 from divide_actions import divide_actions
 from return_data import return_data
 from save_data import save_data
+from unwrapping_gifts import unwrapping_gifts
+from select_gifts import select_gifts
+from save_data2 import save_data2
 
 actions = divide_actions()
-data = return_data(actions)
-save_data(data)
+challenge_1 = return_data(actions)
+save_data(challenge_1)
+
+gifts = unwrapping_gifts()
+challenge_2 = select_gifts(gifts)
+save_data2(challenge_2)
