26d20e053d4a5b71e9784ebfd2c1804e84fdc8c9
dienesviktor <dienesviktor@gmail.com>
2021-12-28 14:36:32 +0100

    update read_map

--- delivery/read_map.py
+++ delivery/read_map.py
@@ -1,8 +1,3 @@
-def read_map(file="data_files/planet.map"):
-    positions = {}
+def read_map(file="data_files/planet-small.map"):
     with open(file, 'r') as map:
-        for line_num, line in enumerate(map, start=1):
-            for cell_pos, cell in enumerate(line, start=1):
-                if cell != "\n":
-                    positions[(line_num, cell_pos)] = cell
-    return positions
+        return [cell for line in map for cell in line]
