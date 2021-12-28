b07095878d43c5d9a1a4ae1c2e0dda9894b7de7a
dienesviktor <dienesviktor@gmail.com>
2021-12-28 11:42:18 +0100

    change read_map

--- delivery/read_map.py
+++ delivery/read_map.py
@@ -3,10 +3,6 @@ def read_map(file="data_files/planet.map"):
     with open(file, 'r') as map:
         for line_num, line in enumerate(map, start=1):
             for cell_pos, cell in enumerate(line, start=1):
-                if cell == "H":
-                    positions[(line_num, cell_pos)] = "HEADQUARTER"
-                elif cell == "#":
-                    positions[(line_num, cell_pos)] = "HOME"
-                else:
-                    pass
+                if cell != "\n":
+                    positions[(line_num, cell_pos)] = cell
     return positions
